e25349185a2d9118c09b2cd201eef37aac35a3e9
Raveen K Sharma <raveen08@gmail.com>
2020-08-19 19:04:48 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,13 +1,9 @@
 class Board
   attr_accessor :cells
   
-  @cells = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+  
   
   def reset!
-    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
-    puts "-----------"
-    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
-    puts "-----------"
-    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+    @cells 
   end
 end  
