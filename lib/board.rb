a0ed1679e2dce574027ff09380ab2a6c3127e929
Raveen K Sharma <raveen08@gmail.com>
2020-08-19 19:08:23 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -10,11 +10,8 @@ class Board
   end
   
   def display
-    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
-    puts "-----------"
-    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
-    puts "-----------"
-    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+    @cells
+
   end
   
 end  
